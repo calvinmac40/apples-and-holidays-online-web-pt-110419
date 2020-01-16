7c51707fda287f23b7d75998270b96649d5e86d3
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-16 02:18:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,14 +42,16 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # holiday_hash.map do |key,value|
-  #   if key == season
+   holiday_hash.map do |key,value|
+     if key == season
   #     value[holiday_name] = supply_array
   #     holiday_hash
   #     #binding.pry
   holiday_hash[season][holiday_name] = supply_array
   # end
   # end
+end
+end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
@@ -58,7 +60,7 @@ holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
+  # iterate through holiday_hash and print items such that #your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
   #   New Years: Party Hats
@@ -68,8 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday,item|
       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
-      #binding.pry
-   end
+    end
   end
 end
 
